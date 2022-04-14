6df129565147a302fafef4e598e94ee0780f289e
Nicolas da Silva <nicolas.dasilva88@gmail.com>
2022-04-14 10:40:34 -0300

    discord clase 5

---
+++ Discord_Semana_5/src/CollectionCustom.java
@@ -0,0 +1,24 @@
+
+public class CollectionCustom<T> {
+
+	T[] unArrayT = (T[]) new String[5];
+	
+	
+
+	/**
+	 * 
+	 * @return the number of elements in this Array
+	 * @param size
+	 */
+	public int size() {
+		return unArrayT.length;
+	}
+
+	
+	public void addFirst(T elemento) {
+		
+		
+	}
+	
+	
+}
